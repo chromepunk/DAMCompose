b789e24c60d389bcc74f038baefb4b65c1e9ad95
Joan Piquero <piquerojoan@gmail.com>
2026-09-15 20:37:16 +0200

    07 button good!

--- app/src/main/java/com/example/damcompose/MainActivity.kt
+++ app/src/main/java/com/example/damcompose/MainActivity.kt
@@ -15,12 +15,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.damcompose.ui.theme.DAMComposeTheme
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +45,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    var mensaje = "hola"
+    //var mensaje = "hola"
+    var mensaje by remember{
+        mutableStateOf("Hola")
+
+    }
 
     Column(   //import con alt + enter
         modifier = modifier
