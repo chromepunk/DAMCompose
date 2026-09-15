563de8203bfc9151e02b98ba870f2fa422258fa1
Joan Piquero <piquerojoan@gmail.com>
2026-09-15 20:24:02 +0200

    06 fillMaxWidth()

--- app/src/main/java/com/example/damcompose/MainActivity.kt
+++ app/src/main/java/com/example/damcompose/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,9 +44,17 @@ fun Greeting(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = "Mi primera app en compose",
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Gray)
         )
-        Text("desarrollo de interfaces")
+        Text(
+            text="desarrollo de interfaces",
+            modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp)
+                .fillMaxWidth() // importa el orden?
+            )
         Text("kotlin + jetpack compose")
 
         Row(
