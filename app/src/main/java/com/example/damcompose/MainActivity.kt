bd9d74a24b439fe1c507ef9937fafc84adb88014
Joan Piquero <piquerojoan@gmail.com>
2026-09-15 20:01:58 +0200

    04 layout row

--- app/src/main/java/com/example/damcompose/MainActivity.kt
+++ app/src/main/java/com/example/damcompose/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.damcompose.ui.theme.DAMComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,6 +44,14 @@ fun Greeting(modifier: Modifier = Modifier) {
         )
         Text("desarrollo de interfaces")
         Text("kotlin + jetpack compose")
+
+        Row{
+            Text("Kotlin"
+            //, Modifier.padding(end = 16.dp)
+                //o ros(horizontalArrangement = Arrangement.spacedBy(16.dp) "quiero que haya 16 dp entre los elementos de este row
+                     )
+            Text("Compose")
+        }
     }
 }
 
